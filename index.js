cad0d4767bcf4a6bc3a39f1185369e805ff90c1e
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-07 00:03:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,10 +62,10 @@ var Active = [false, false]
 client.on("ready", () => console.log("I'm onlin and ready!"));
 
 setTimeout(() => {
-  if(member.roles.get('594185021389144066')) message.author.send(`Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked`)
+  if(member.author.id('594185021389144066')) message.author.send(`**Please To get in you have to register your Social Club name on this server by doing >register Then your social club name in the #register chat, if you dont in the next minutes you will be kicked**`)
 }, 30000);
 setTimeout(() => {
-  if(member.roles.get('594185021389144066')) member.kick('Failed to register')
+  if(member.author.id('594185021389144066')) member.kick('Failed to register')
 }, 60000);
 
 
