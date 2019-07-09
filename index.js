ec4c74ad15481ae15d3012c1d54b70771b290de1
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-09 15:48:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ fs.readdir('./events/', (err, files) => {
 
   });
 
-  setTimeout((member, id,  ) => {
+  setTimeout(() => {
     if(member.author.id('594185021389144066')) message.author.send(`**Please To get in you have to register your Social Club name on this server by doing >register Then your social club name in the #register chat, if you dont in the next minutes you will be kicked**`)
   }, 30000);
   setTimeout(() => {
