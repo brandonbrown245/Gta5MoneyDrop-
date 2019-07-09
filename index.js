13106165914372b2379f3985e6052938db6a42a2
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-09 15:48:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,13 +57,18 @@ fs.readdir('./events/', (err, files) => {
 
   });
 
+  setTimeout((member, id,  ) => {
+    if(member.author.id('594185021389144066')) message.author.send(`**Please To get in you have to register your Social Club name on this server by doing >register Then your social club name in the #register chat, if you dont in the next minutes you will be kicked**`)
+  }, 30000);
+  setTimeout(() => {
+    if(member.author.id('594185021389144066')) member.kick('Failed to register')
+  }, 60000);
+
+
 var Queue = new Array();
 var Active = [false, false]
 
 client.on("ready", () => console.log("I'm onlin and ready!"));
-
-
-
 
 client.on("message", async message  => {
 
