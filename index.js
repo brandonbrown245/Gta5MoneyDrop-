b5f9216e21b5fd40dfd13bdc4ae8c41c26bda8f9
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-23 22:50:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -439,7 +439,7 @@ setTimeout(() => {
 setTimeout(() => {
   if(member.roles.get('594185021389144066')) member.kick('')
   member.guild.channels.get("594183922049482778").send(`${member.user.tag} has been Kicked from the server Because he didnt register his Social Club Name`);
-  member.sendMessage(`**You have been Kicked in ${message.guild.name}\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
+  member.sendMessage(`**You have been Kicked in **Gta Money Drop**\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
 }, 60000);
 
 })
