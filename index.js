061cb323e52ad535a1f4543e6f60ae17ea495fa6
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-24 16:41:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ var command = args.shift().toLowerCase()
 let Clogs = message.guild.channels.find(x => x.name == "logs");
 
 setTimeout(() => {
-  if(member.roles.get('594185021389144066')) member.sendMessage(`**Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked**`)
+  if(!message.member.roles.find(r => r.name == "Unregistered")) return member.sendMessage(`**Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked**`)
  }, 30000);
  
  setTimeout(() => {
-   if(member.roles.get('594185021389144066')) member.kick('')
+  if(!message.member.roles.find(r => r.name == "Money Unregistered")) return  member.kick('')
    member.guild.channels.get("594183922049482778").send(`${member.user.tag} has been Kicked from the server Because he didnt register his Social Club Name`);
    member.sendMessage(`**You have been Kicked in **Gta Money Drop**\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
  }, 60000);
