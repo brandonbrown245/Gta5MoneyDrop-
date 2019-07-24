66df28759fed35c84c7697ff79bf9c702a953ed2
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-24 22:41:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -429,6 +429,17 @@ let avatar = await loadImage(avatar1.body);
 await ctx.drawImage(avatar, 52,46,114,104)
 member.guild.channels.get("594183892651737108").send({files : [await canvas.toBuffer()]}) 
 
+setTimeout(() => {
+  if(member.roles.get('594185021389144066')) member.sendMessage(`**Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked**`)
+ }, 30000);
+ 
+ 
+ setTimeout(() => {
+   if(member.roles.get('594185021389144066')) member.kick('')
+   member.guild.channels.get("594183922049482778").send(`${member.user.tag} has been Kicked from the server Because he didnt register his Social Club Name`);
+   member.sendMessage(`**You have been Kicked in **Gta Money Drop**\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
+ }, 60000);
+ 
 });
 
 client.on("guildMemberUpdate", async (oldMember, newMember) => {
