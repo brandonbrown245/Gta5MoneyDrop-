8ff519c768fa1f76d70015b8a7e264cd66ebd8f8
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-07-24 16:36:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,22 +64,7 @@ var Active = [false, false]
 
 client.on("ready", () => console.log("I'm onlin and ready!"));
 
-
-
-
 client.on("message", async message  => {
-
-
-  setTimeout(() => {
-    if(member.roles.get('594185021389144066')) member.sendMessage(`**Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked**`)
-   }, 30000);
-   
-   setTimeout(() => {
-     if(member.roles.get('594185021389144066')) member.kick('')
-     member.guild.channels.get("594183922049482778").send(`${member.user.tag} has been Kicked from the server Because he didnt register his Social Club Name`);
-     member.sendMessage(`**You have been Kicked in **Gta Money Drop**\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
-   }, 60000);
-   
 
 if(message.author.bot) return;
 if(!message.content.startsWith(config.PREFIX)) return;
@@ -91,7 +76,15 @@ var command = args.shift().toLowerCase()
 
 let Clogs = message.guild.channels.find(x => x.name == "logs");
 
-
+setTimeout(() => {
+  if(member.roles.get('594185021389144066')) member.sendMessage(`**Please, To get in you have to register your Social Club name on this server by doing **>register Then your social club name** in the #register chat, if you dont in the next minutes you will be kicked**`)
+ }, 30000);
+ 
+ setTimeout(() => {
+   if(member.roles.get('594185021389144066')) member.kick('')
+   member.guild.channels.get("594183922049482778").send(`${member.user.tag} has been Kicked from the server Because he didnt register his Social Club Name`);
+   member.sendMessage(`**You have been Kicked in **Gta Money Drop**\n**Reason**: Didnt Register Social Club Name On The Server\n**`)
+ }, 60000);
 
 if(command == "register") {
 message.delete();
