b298cd9c24d5d54dd5d61c7ccdf378111c72f33a
Brandon <bb5675773@gmail.com>
2020-01-13 14:51:12 +0000

    Delete ready.js

--- Commands/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = client => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    const verify = client.channels.find(c => c.name === 'rules');
-
-    const fetchedMessages = [verify];
-    fetchedMessages.forEach(c => {
-    });
-};
