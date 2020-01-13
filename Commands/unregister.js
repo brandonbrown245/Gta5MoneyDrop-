bab9fb405f6ca18ed94e295b41504053bc1ae5e7
Brandon <bb5675773@gmail.com>
2020-01-13 12:54:20 +0000

    Create unregister.js

---
+++ Commands/unregister.js
@@ -0,0 +1,27 @@
+const SQLite = require('sqlite')
+const fs = require('fs')
+module.exports = (client, message, args) => {
+
+    SQLite.open('./Data/userData.sqlite')
+    .then(async sql => {
+        let user = await sql.get(`SELECT * FROM users WHERE id = ${message.author.id}`)
+        if(!user) return message.reply("You are not registered!")
+        sql.run(`DELETE FROM users WHERE id = ${message.author.id}`)
+        .then(() => {
+            message.member.addRole(client.config.unregistered)
+            message.member.removeRole(client.config.registered)
+            let logsChannel = client.channels.get(client.config.logschannel)
+            message.channel.send(`Successfully unregistered!`)
+            .then(msg => msg.delete(5000))
+            logsChannel.send(`**[!]** ${message.author} was unregistered!`)
+
+            let data = JSON.parse(fs.readFileSync('./Data/tempRegs.json'))
+            data.push({time : +new Date() , id: message.member.id})
+            fs.writeFileSync('./Data/tempRegs.json', JSON.stringify(data,null,2))
+        })
+        .catch((e) => {
+            console.log(e)
+            message.reply('Oops, there was an error trying to delete your info!')
+        })
+    })
+}
